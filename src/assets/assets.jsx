73c9996c603cbff3159f1792fe5d5d573af12840
syama777 <syamasundarb071@gmail.com>
2025-01-27 17:01:51 +0530

    admin

---
+++ src/assets/assets.jsx
@@ -0,0 +1,23 @@
+import parcel from './parcel.png'
+import booking from './booking.png'
+import admin from './admin.png'
+import upload from './upload.png'
+import add from './add.png'
+import order from './order.png'
+import logoutt from './logoutt.png'
+import remove from './remove.png'
+import rating from './rating.png'
+
+export const assets ={
+    parcel,
+    booking,
+    admin,
+    upload,
+    add,
+    order,
+    logoutt,
+    remove,
+    rating
+}
+//axios -network requst(get,post,put,delet)
+//react-toastify  user notification  use only
